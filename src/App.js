a44518c04d655a4fea64a8b07a31da22f81a6b64
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-11-03 15:42:38 -0400

    login.js and createuser.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import {useState, useEffect} from 'react';
-
+import LoginForm from './LoginForm'
+import NewUserForm from './NewUserForm'
 
 const App = () => {
-  
+  const [toggleLogin, setToggleLogin] = useState(true);
+  const [toggleError, setToggleError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [toggleLogout, setToggleLogout] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+
+  const handleCreateUser = (userObj) => {
+    axios.post('http://localhost:3001/users/new', userObj)
+    .then((res) => {
+      if (res.data.username) {
+        console.log(res);
+        setToggleError(false)
+        setErrorMessage('')
+        setCurrentUser(res.data)
+        handleToggleLogout()
+      } else {
+        setErrorMessage(res.data)
+        setToggleError(true)
+      }
+    })
+  }
 
   return (
     <div>
