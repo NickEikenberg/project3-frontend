f62fd248b5ef1d2c57b19a681f4e270d10333500
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-11-03 16:44:19 -0400

    user login form complete

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,12 +7,33 @@ const userLoginForm = (props) => {
   const triggerLogin = (event) => {
     event.preventDefault();
     let userObj = { username: username, password: password };
-    // props.handleLogin(userObj)
+    props.handleLogin(userObj);
   };
 
   return (
     <div className="loginFormContainer">
       <h1>Login</h1>
+      <form onSubmit={triggerLogin}>
+        <h2>Username</h2>
+        <input
+          type="text"
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <h2>Password</h2>
+        <input
+          type="password"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        {props.toggleError ? <h5>{props.errorMessage}</h5> : null}
+
+        <input type="submit" value="login" />
+      </form>
     </div>
   );
 };
+
+export default userLoginForm;
