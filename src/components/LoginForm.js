c012d34d49db03749da20b1527ca3e03110ea261
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-11-03 16:30:27 -0400

    User login form started

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 
 const userLoginForm = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const triggerLogin = (event) => {
+    event.preventDefault();
+    let userObj = { username: username, password: password };
+    // props.handleLogin(userObj)
+  };
+
   return (
     <div className="loginFormContainer">
       <h1>Login</h1>
