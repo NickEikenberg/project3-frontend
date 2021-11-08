4ab8efe189606d825db32ae6140c4bd74e5a3fe7
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-11-08 08:52:32 -0500

    changes localhost to thawing scrubland

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -37,7 +37,7 @@ const UserProfile = ({
   const updateUsernameAndAvatar = (userObj) => {
     axios
       .put(
-        `http://localhost:3001/users/${user.username}`,
+        `http://thawing-scrubland-60943.herokuapp.com/users/${user.username}`,
         userObj
       )
       .then((res) => {
