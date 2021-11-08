c5ee974773be9265fabe5e5f8b0e4c199d6e2ebc
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-11-08 13:05:59 -0500

    begin to start

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -26,7 +26,7 @@ const Rules = () => {
           <p className="mt-1 underline">How it works:</p>
           <ol>
             <li>1. Join the same room with a group of friends. <span className="text-xs italic">We recommend 4+ participants. The more the merrier. Using an external voice chat can improve the experience.</span></li>
-            <li>2. Once everyone is in the same room, click Begin Game. <span className="text-xs italic">During the game, you can only type when it is you turn. Write the next sentence of the story, but you can only see the previous sentence!</span></li>
+            <li>2. Once everyone is in the same room, click Start. <span className="text-xs italic">During the game, you can only type when it is you turn. Write the next sentence of the story, but you can only see the previous sentence!</span></li>
             <li>3. When you think the story has reached a conclusion, click End Game. <span className="text-xs italic">The entire story will be revealed to all participants, and you can laugh at the absurdity together.</span></li>
           </ol>
         </div>
