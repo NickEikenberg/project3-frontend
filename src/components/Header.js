043d0cca7e852b4af62557c4cd21a24307023073
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-11-08 19:39:43 -0500

    added favicon and banana logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,12 @@
-const Header = ({ showUserProfile, user }) => {
+const Header = () => {
   return (
     <header className="w-full flex justify-between items-center bg-white py-4 px-4 shadow-sm">
-      <h1 className="text-3xl text-center w-1/3">Tinier Bananas</h1>
-      <button className="justify-self-end flex justify-end">
-        <span className="cursor-pointer">
-          {user.username ? (
-            <ion-icon
-              name="person-circle-outline"
-              onClick={showUserProfile}
-            ></ion-icon>
-          ) : (
-            <ion-icon name="person-circle-outline"></ion-icon>
-          )}
-        </span>
-      </button>
+      <h1 className="text-3xl text-center">Tinier Bananas</h1>
+      <img
+        className="justify-self-end flex justify-end h-14 border border-black border-opacity-75 rounded-lg"
+        src="../tiny-bananas.jpeg"
+        alt="tiny bananas"
+      />
     </header>
   );
 };
